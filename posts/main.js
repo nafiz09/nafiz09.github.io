16a2bfce86da0dd7c7ff59030553b47bf193e426
nafiz09 <nafizislam09@gmail.com>
2024-05-23 16:56:24 +0600

    blog1 posted

---
+++ posts/main.js
@@ -0,0 +1,50 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+function copyCode(id) {
+    const codeElement = document.getElementById(id);
+    const text = codeElement.textContent;
+    navigator.clipboard.writeText(text).then(() => {
+      // Optional: Show a success message to the user
+      console.log("Code copied to clipboard!");
+    }, (err) => {
+      console.error('Failed to copy code: ', err);
+    });
+  }
+  
+
+
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+
+// sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
